95a37774c5674a5fb86fd4d98738979ca9088569
leohx2 <leohx2@users.noreply.github.com>
2021-01-29 19:02:48 +0000

    ex02

---
+++ C08/ex02/ft_abs.h
@@ -0,0 +1,6 @@
+#ifndef FT_ABS
+#define FT_ABS
+
+#define ABS(Value) (Value < 0 ) ? (Value * -1) : (Value)
+
+#endif
---
+++ C08/test.c
@@ -0,0 +1,8 @@
+#include "ft_abs.h"
+#include <stdio.h>
+int main()
+{
+    int test = 45555;
+    printf("%i", ABS(test));
+
+}
