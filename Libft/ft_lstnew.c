5c2746a7871cebba3b6dc87a5bf2e63766e21149
leohx2 <leohx2@users.noreply.github.com>
2021-02-17 20:41:23 +0000

    go

---
+++ Libft/ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrosendo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/17 18:12:47 by lrosendo          #+#    #+#             */
+/*   Updated: 2021/02/17 18:12:47 by lrosendo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*to_return;
+
+	if (!(to_return = (t_list *)malloc(sizeof(to_return))))
+		return (NULL);
+	to_return->content = content;
+	to_return->next = 0;
+	return(to_return);
+}
