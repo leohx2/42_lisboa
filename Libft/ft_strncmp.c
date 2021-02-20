8dc1c7c274ed774a93272ef1671526565f2df3e2
Leonardo Rosendo silva <lrosendo@c1r20s1.42lisboa.com>
2021-02-20 17:27:41 +0000

    linhas

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lrosendo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 13:49:22 by lrosendo          #+#    #+#             */
-/*   Updated: 2021/02/20 14:45:31 by lrosendo         ###   ########.fr       */
+/*   Updated: 2021/02/20 17:27:17 by lrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,15 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	s1_aux = (unsigned char*)s1;
 	s2_aux = (unsigned char*)s2;
 	aux = 0;
-	if (n == 0)
-		return (0);
-	if (ft_strlen(s1) && !ft_strlen(s2))
-		return (1);
 	if (!ft_strlen(s1) && ft_strlen(s2))
 		return (-1);
-	while (aux < n && s1_aux[aux] && s1_aux[aux])
+	while (aux < n && s1_aux[aux] && s2_aux[aux])
 	{
 		if (s1_aux[aux] != s2_aux[aux])
 			return (s1_aux[aux] - s2_aux[aux]);
 		aux++;
 	}
+	if (s1_aux[aux] != s2_aux[aux] && aux < n)
+			return (s1_aux[aux] - s2_aux[aux]);
 	return (0);
 }
