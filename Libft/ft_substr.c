70961b4d5d5013de1420bde0884451844badd85e
Leonardo Rosendo silva <lrosendo@c1r7s6.42lisboa.com>
2021-02-23 21:58:47 +0000

    final sub

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lrosendo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 19:28:19 by lrosendo          #+#    #+#             */
-/*   Updated: 2021/02/20 17:38:04 by lrosendo         ###   ########.fr       */
+/*   Updated: 2021/02/23 18:37:27 by lrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,20 @@
 char	*ft_substr(char const *s, unsigned int start,
 unsigned int len)
 {
-	char				*str_aux;
-	char				*to_return;
-	unsigned int		aux;
+	char			*str_aux;
+	unsigned int	aux;
+	int				alloc;
 
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < len + start)
+		alloc = ft_strlen(s) - start;
+	else
+		alloc = len;
 	if (ft_strlen(s) < start || len == 0)
-		return (to_return = (char*)ft_calloc(2, 1));
-	if (!(str_aux = (char*)ft_calloc((len - start + 2), sizeof(char))))
-		return(NULL);
+		return (str_aux = (char*)ft_calloc(2, 1));
+	if (!(str_aux = (char*)ft_calloc(alloc + 1, sizeof(char))))
+		return (NULL);
 	aux = 0;
 	while (aux < len && s[start])
 	{
@@ -30,9 +36,5 @@ unsigned int len)
 		start++;
 		aux++;
 	}
-	str_aux[aux] = 0;
-	to_return = ft_strdup(str_aux);
-	free(str_aux);
-	return (to_return);
+	return (str_aux);
 }
-
